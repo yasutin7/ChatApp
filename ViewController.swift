d241f4ffc9483268d630b74c9cbc02f5faef86d8
yasunari <heaty2525@yahoo.co.jp>
2020-11-27 18:39:24 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 藤原康就 on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
